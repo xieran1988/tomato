dadce9fe81c4128e366ebc895d2856c53a6f9cfe
root <root@li476-71.members.linode.com>
2012-12-23 07:31:14 +0000

    modify add.sh

--- sumup.sh
+++ sumup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Total user"
-echo "select count(*) from user" | mysql tomato -uroot | sed '1d'
+echo "select count(*) from user" | mysql tomato -uroot -p1 | sed '1d'
 
 echo "Valid user"
-echo "select count(distinct(email)) from entry" | mysql tomato -uroot | sed '1d'
+echo "select count(distinct(email)) from entry" | mysql tomato -uroot -p1 | sed '1d'
 
 echo "List"
-echo "select distinct(email), count(*) from entry" | mysql tomato -uroot | sed '1d'
+echo "select distinct(email), count(*) from entry" | mysql tomato -uroot -p1 | sed '1d'
 
